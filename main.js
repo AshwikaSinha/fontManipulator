fbfa1d8c116b69c7a7225b0fd950d80acef5ef0f
AshwikaSinha <73628211+AshwikaSinha@users.noreply.github.com>
2021-07-06 17:35:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -15,6 +18,10 @@ function modelLoaded() {
 
 function draw() {
     background('#FFFF00');
+    document.getElementById("font_size").innerHTML = "Font size of the Text will be =  " + difference +"px"
+    text('Ash', 50, 400);
+    textSize(difference)
+    fill('#F90093');
 }
 
 function gotPoses(results)
@@ -22,5 +29,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
     }
 }
